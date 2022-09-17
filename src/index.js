101965b45360f222fd91e0b5d98c478fcb82eed7
Wakeel Furqan Ahmed <Wakeelfahmed@gmail.com>
2022-09-18 02:27:28 +0500

    Routing Setup

--- src/index.js
+++ src/index.js
@@ -5,15 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-/*import "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/free-solid-svg-icons"
-import "@fortawesome/react-fontawesome"*/
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
