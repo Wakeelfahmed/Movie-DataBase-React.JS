7338ae02480d744f7e1f1e2d6c2774cfff0a4f1e
Wakeel Furqan Ahmed <Wakeelfahmed@gmail.com>
2022-09-19 01:54:01 +0500

    Login Form setup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import React, { Component } from "react";
-import {Route, Redirect, Switch } from "react-router-dom"
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
-import MovieForm from './components/movieForm';
-import Customers from './components/customers';
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers";
 import NavBar from "./components/navBar";
-import Rentals from './components/rentals';
-import NotFound from './components/notFound';
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import LoginForm from "./components/loginForm";
 import "./App.css";
 
 class App extends Component {
-  render(){
+  render() {
     return (
       <React.Fragment>
-      <NavBar></NavBar>
+        <NavBar></NavBar>
         <main className="container">
           <Switch>
-          <Route path="/movies/:id" component={MovieForm}/>
-            <Route path="/movies" component={Movies}/>
-            <Route path="/customers" component={Customers}/>
-            <Route path="/rentals" component={Rentals}/>
-            <Route path="/not-found" component={NotFound}/>
-            <Redirect from="/" exact to="/movies"/>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path=" /movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
             <Redirect to="/not-found" />
           </Switch>
         </main>
-        </React.Fragment>
+      </React.Fragment>
     );
   }
 }
-export default App; 
+export default App;
